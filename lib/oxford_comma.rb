ee0ecb464b8bfac8fd52ef8fb4afc5113c5857d1
taroj khan <tarujkhan@yahoo.com>
2020-03-04 19:13:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,11 @@
 def oxford_comma(array)
 if array.length == 2 
   return "#{array[0]} and #{array[1]}"
-#array.insert(-2, "and").to_s
+
 end
+if array.length > 2
+array.insert(-2, "and").to_s
+end 
 array.join(", ")
 end
-
 
