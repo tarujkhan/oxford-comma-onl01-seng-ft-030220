2836323906dcf566037d8509171a3a257fbdaee7
taroj khan <tarujkhan@yahoo.com>
2020-03-04 19:20:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,12 @@
 
 def oxford_comma(array)
+if array.length == 2 
+ return "#{array[0]} and #{array[1]}"
 
-array << and " "
-array
 end
-
+if array.length > 2
+array[-1].insert(0, "and ").to_s
+end 
+array.join(", ")
+end
 
